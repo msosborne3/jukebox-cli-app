73d48b48a2bad4c37204bc9eb3f501a7b1501b59
msosborne3 <msosborne@ymail.com>
2016-10-12 23:13:55 -0500

    Used bundle gem to create the gem structure

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in jukebox-cli-app.gemspec
+gemspec
---
+++ lib/jukebox/cli/app.rb
@@ -0,0 +1,9 @@
+require "jukebox/cli/app/version"
+
+module Jukebox
+  module Cli
+    module App
+      # Your code goes here...
+    end
+  end
+end
