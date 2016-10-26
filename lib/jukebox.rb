211c480345484a301d1dd2962727c04ea0de1f70
msosborne3 <msosborne@ymail.com>
2016-10-26 17:50:27 -0500

    added require lib/jukebox/concert

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,7 +1,6 @@
 require_relative "./jukebox/version"
-
-module Jukebox
-
-end
-
 require_relative './jukebox/cli'
+require_relative './jukebox/concert'
+
+#File acts as environment so that it is the file that
+#jukebox can require and it can require everything else
