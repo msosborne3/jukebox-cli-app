19055678f5ac209b55daf3a7fb88760d98d167ae
msosborne3 <msosborne@ymail.com>
2016-10-26 17:20:14 -0500

    updated the cli controller with the fake data

--- lib/jukebox/cli.rb
+++ lib/jukebox/cli.rb
@@ -2,13 +2,13 @@
 class Jukebox::CLI
 
   def call
+    puts "Welcome to Jukebox!"
     list_concerts
     menu
     goodbye
   end
 
   def list_concerts
-    puts "Welcome to Jukebox!"
     puts "Here is a list of upcoming concerts in the Memphis area:\n\n"
     puts <<-DOC.gsub /^\s*/, ''
     1. Panic at the Disco
@@ -17,23 +17,27 @@ class Jukebox::CLI
   end
 
   def menu
-    puts "\nPlease enter the number corresponding to the concert you\n"\
-    "want to hear more about."
     input = nil
     while input != "exit"
+      puts "\nPlease enter the number corresponding to the concert you\n"\
+      "want to hear more about, type list to see the concerts again, or\n"\
+      "type exit to leave Jukebox: "
+
       input = gets.strip.downcase
 
       case input
       when "1"
-        puts "More info on P!ATD..."
+        puts "More info on P!ATD...\n"
       when "2"
-        puts "More info on Twenty One Pilots..."
+        puts "More info on Twenty One Pilots...\n"
+      when "list"
+        list_concerts
       end
     end
   end
 
   def goodbye
-    puts "Goodbye! Thanks for using Jukebox! Check back soon for more concerts!"
+    puts "\nGoodbye! Thanks for using Jukebox! Check back soon for more concerts!"
   end
 
 end
