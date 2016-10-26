669df94951822fd717559aa41c2c31816b46ee28
msosborne3 <msosborne@ymail.com>
2016-10-26 03:10:29 -0500

    Updated the CLI controller with sample output

--- lib/jukebox/cli.rb
+++ lib/jukebox/cli.rb
@@ -2,7 +2,13 @@
 class Jukebox::CLI
 
   def call
-    puts "Today's concerts "
+    puts "Welcome to Jukebox!"
+    puts "Here is a list of upcoming concerts in the Memphis area:"
+
+    #List concerts
+
+    puts "Please enter the number corresponding to the concert you "\
+    "want to hear more about."
   end
 
 end
